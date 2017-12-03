ecd4396bcec7d7885e987fb1be701f2afbb03f0f
Jeremy Barthe <jeremy.barthe@gmail.com>
2017-12-03 15:47:38 +0100

    improved PHPSpec tests and tried to fix Behat tests

--- packages/meetup-api/spec/Meetup/Http/Plugin/AddApiKeyPluginSpec.php
+++ packages/meetup-api/spec/Meetup/Http/Plugin/AddApiKeyPluginSpec.php
@@ -2,9 +2,14 @@
 
 namespace spec\Meetup\Http\Plugin;
 
+use Http\Client\Common\Plugin;
+use Http\Client\Promise\HttpFulfilledPromise;
+use Http\Promise\FulfilledPromise;
 use Meetup\Http\Plugin\AddApiKeyPlugin;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\UriInterface;
 
 class AddApiKeyPluginSpec extends ObjectBehavior
@@ -19,27 +24,48 @@ class AddApiKeyPluginSpec extends ObjectBehavior
         $this->shouldHaveType(AddApiKeyPlugin::class);
     }
 
+    function it_is_a_plugin()
+    {
+        $this->shouldImplement(Plugin::class);
+    }
+
     function it_should_add_api_key_to_query_string(
         RequestInterface $request,
+        ResponseInterface $response,
         UriInterface $uri
     ) {
         $request->getUri()->shouldBeCalled()->willReturn($uri);
         $uri->getQuery()->shouldBeCalled()->willReturn('');
         $uri->withQuery('key=secret_key')->shouldBeCalled()->willReturn($uri);
-        $request->withUri($uri)->shouldBeCalled();
+        $request->withUri($uri)->shouldBeCalled()->willReturn($request);
 
-        $this->handleRequest($request, function () {}, function () {});
+        $next = function (RequestInterface $receivedRequest) use($request, $response) {
+            if (Argument::is($request->getWrappedObject())->scoreArgument($receivedRequest)) {
+                return new HttpFulfilledPromise($response->getWrappedObject());
+            }
+        };
+
+        $promise = $this->handleRequest($request, $next, function () {});
+        $promise->shouldReturnAnInstanceOf(HttpFulfilledPromise::class);
     }
 
     function it_should_add_api_key_to_existing_query_string(
         RequestInterface $request,
+        ResponseInterface $response,
         UriInterface $uri
     ) {
         $request->getUri()->shouldBeCalled()->willReturn($uri);
         $uri->getQuery()->shouldBeCalled()->willReturn('query=lorem');
         $uri->withQuery('query=lorem&key=secret_key')->shouldBeCalled()->willReturn($uri);
-        $request->withUri($uri)->shouldBeCalled();
+        $request->withUri($uri)->shouldBeCalled()->willReturn($request);
 
-        $this->handleRequest($request, function () {}, function () {});
+        $next = function (RequestInterface $receivedRequest) use($request, $response) {
+            if (Argument::is($request->getWrappedObject())->scoreArgument($receivedRequest)) {
+                return new HttpFulfilledPromise($response->getWrappedObject());
+            }
+        };
+
+        $promise = $this->handleRequest($request, $next, function () {});
+        $promise->shouldReturnAnInstanceOf(HttpFulfilledPromise::class);
     }
 }
